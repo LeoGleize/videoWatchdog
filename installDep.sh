7b79bf966a608336d4dea1cff0b28bfcbee179eb
Filipe Caldas <fcaldas@canal-plus.fr>
2015-10-27 11:32:31 +0100

    Added dependencies installer

---
+++ installDep.sh
@@ -0,0 +1,11 @@
+sudo apt-get install g++-4.8 g++ git make libboost1.54-all-dev libssl-dev cmake
+sudo apt-get install libopencv-dev
+git clone https://git.codeplex.com/casablanca
+cd casablanca/Release
+mkdir build.release
+cd build.release
+CXX=g++-4.8 cmake .. -DCMAKE_BUILD_TYPE=Release
+make
+sudo make install
+echo "All dependencies have been installed"
+echo "You can now build R7IntensityPro running 'make'"
