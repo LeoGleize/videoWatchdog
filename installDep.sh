ffa4f2ed742bdad110547bc024fd7dd04096ade3
Filipe Caldas <fcaldas@canal-plus.fr>
2015-11-16 14:06:06 +0100

    Fixed installer

--- installDep.sh
+++ installDep.sh
@@ -1,7 +1,9 @@
 sudo apt-get install g++-4.8 g++ git make libboost1.54-all-dev libssl-dev cmake gcc
 sudo apt-get install libopencv-dev
 sudo apt-get install apache2 php5 php5-curl
-sudo dpkg -i BlackmagicSoftware/deb/amd64/*.deb
+sudo dpkg -i BlackmagicSoftware/deb/amd64/desktopvideo_10.5a17_amd64.deb
+sudo dpkg -i BlackmagicSoftware/deb/amd64/desktopvideo-gui_10.5a17_amd64.deb
+sudo dpkg -i BlackmagicSoftware/deb/amd64/mediaexpress_3.4.1a2_amd64.deb
 sudo chmod -R 777 /var/www
 git clone https://git.codeplex.com/casablanca
 cd casablanca/Release
